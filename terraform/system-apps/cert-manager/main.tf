93bff657a3d392df5ca0f2e0af2af006fc9dcc65
Victor Gartvich <vgartvich@wallarm.com>
2020-04-24 20:42:23 -0700

    Add cert-manager service

---
+++ terraform/system-apps/cert-manager/main.tf
@@ -0,0 +1,31 @@
+
+
+resource "helm_release" "cert-manager-crds" {
+  name  = "tf-cert-manager-crds"
+  chart = "cert-manager-crds"
+}
+
+data "helm_repository" "jetstack" {
+  name = "jetstack"
+  url  = "https://charts.jetstack.io"
+}
+
+resource "helm_release" "cert-manager" {
+  name  = "tf-cert-manager"
+  chart = "cert-manager"
+  version    = "0.14.2"
+  repository = data.helm_repository.jetstack.metadata[0].name
+  depends_on = [helm_release.cert-manager-crds]
+}
+
+
+resource "helm_release" "cert-manager-issuer" {
+  name  = "tf-cert-manager-issuer"
+  chart = "cert-manager-issuer"
+  depends_on = [helm_release.cert-manager]
+
+  set_string {
+    name = "acme_email"
+    value = var.acme_email
+  }
+}
